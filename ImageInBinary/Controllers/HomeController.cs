6152c46ec74acc01f6875f524c2be635052871c9
inazarm <inazar.m@gmail.com>
2020-11-27 17:47:24 +0500

    complete project of save image into database in binary format

---
+++ ImageInBinary/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImageInBinary.Models;
+
+namespace ImageInBinary.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            TestDBEntities entities = new TestDBEntities();
+            return View(entities.tblFiles.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            byte[] bytes;
+            using (BinaryReader br = new BinaryReader(postedFile.InputStream))
+            {
+                bytes = br.ReadBytes(postedFile.ContentLength);
+            }
+            TestDBEntities entities = new TestDBEntities();
+            entities.tblFiles.Add(new tblFile
+            {
+                Name = Path.GetFileName(postedFile.FileName),
+                ContentType = postedFile.ContentType,
+                Data = bytes
+            });
+            entities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
